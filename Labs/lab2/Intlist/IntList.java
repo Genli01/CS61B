61ea3547b6c265eac2698cc55ff5916010e88516
Genli01 <ligen01@gmail.com>
2017-04-05 21:42:10 -0700

    Implemented destructive and non-destructive methods of catenate

--- Labs/lab2/Intlist/IntList.java
+++ Labs/lab2/Intlist/IntList.java
@@ -81,7 +81,12 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        IntList ptr = A;
+        while (ptr.rest != null) {
+            ptr = ptr.rest;
+        }
+        ptr.rest = B;
+        return A;
     }
 
     /**
@@ -89,8 +94,18 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
-        //TODO:  fill in method
-        return null;
+        IntList res = new IntList(A.first, null);
+        IntList ptr = res;
+        A = A.rest;
+        while (A != null) {
+            ptr.rest = new IntList(A.first, null);
+            A = A.rest;
+            ptr = ptr.rest;
+        }
+
+        ptr.rest = B;
+
+        return res;
     }
 
 
--- Lectures/out/production/inheritance3/TODO.md
+++
@@ -1,1 +0,0 @@
-Create a new interface that allows the Maximizer.max method to do its job on ANY object implementing that interface.
