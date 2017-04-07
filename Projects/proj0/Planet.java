21bd5a4787bbfa0690ec021b1ba0a3585ec95c1a
Genli01 <ligen01@gmail.com>
2017-04-06 23:52:13 -0700

    finished Planet.java

--- Projects/proj0/Planet.java
+++ Projects/proj0/Planet.java
@@ -1,2 +1,28 @@
 public class Planet {
+    double xxPos; //Its current x position
+    double yyPos; //Its current y position
+    double xxVel; //Its current velocity in the x direction
+    double yyVel; //Its current velocity in the y direction
+    double mass; //Its mass
+    String imgFileName; //The name of an image in the images directory that depicts the planet
+    public Planet(double xP, double yP, double xV,
+                  double yV, double m, String img) {
+        xxPos = xP;
+        yyPos = yP;
+        xxVel = xV;
+        yyVel = yV;
+        yyVel = yV;
+        mass = m;
+        imgFileName = img;
+    }
+    public Planet(Planet p) {
+        xxPos = p.xxPos;
+        yyPos = p.yyPos;
+        xxVel = p.xxVel;
+        yyVel = p.yyVel;
+        yyVel = p.yyVel;
+        mass = p.mass;
+        imgFileName = p.imgFileName;
+    }
+
 }
