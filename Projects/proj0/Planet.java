610d72fc3a5e951e0d0d86ca10c6d7b73bf66132
Genli01 <ligen01@gmail.com>
2017-04-07 20:58:41 -0700

    Added force calculations

--- Projects/proj0/Planet.java
+++ Projects/proj0/Planet.java
@@ -5,6 +5,8 @@ public class Planet {
     double yyVel; //Its current velocity in the y direction
     double mass; //Its mass
     String imgFileName; //The name of an image in the images directory that depicts the planet
+    public static final double G = 6.67e-11;
+
     public Planet(double xP, double yP, double xV,
                   double yV, double m, String img) {
         xxPos = xP;
@@ -28,6 +30,40 @@ public class Planet {
     public double calcDistance(Planet p) {
         return Math.sqrt((this.xxPos - p.xxPos) * (this.xxPos - p.xxPos) +
                 (this.yyPos - p.yyPos) * (this.yyPos - p.yyPos));
+    }
+
+    public double calcForceExertedBy(Planet p) {
+        double r = calcDistance(p);
+        return G * this.mass * p.mass / r / r;
+    }
+
+    public double calcForceExertedByX(Planet p) {
+        //Fx = F * dx / r
+        return calcForceExertedBy(p) * (p.xxPos - this.xxPos) / calcDistance(p);
+    }
+
+    public double calcForceExertedByY(Planet p) {
+        return calcForceExertedBy(p) * (p.yyPos - this.yyPos) / calcDistance(p);
+
+    }
+    public double calcNetForceExertedByX(Planet[] planets) {
+        double netForceExertedByX = 0.0;
+        for (Planet p : planets) {
+            if (!p.equals(this)) {
+                netForceExertedByX += calcForceExertedByX(p);
+            }
+        }
+        return netForceExertedByX;
+    }
+
+    public double calcNetForceExertedByY(Planet[] planets) {
+        double netForceExertedByY = 0.0;
+        for (Planet p : planets) {
+            if (!p.equals(this)) {
+                netForceExertedByY += calcForceExertedByY(p);
+            }
+        }
+        return netForceExertedByY;
     }
 
 
