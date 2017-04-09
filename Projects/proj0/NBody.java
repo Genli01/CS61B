a658faca22d73cc7b66f539d75615212dbecd551
Genli01 <ligen01@gmail.com>
2017-04-09 00:34:38 -0700

    Part 1 of NBody

--- Projects/proj0/NBody.java
+++ Projects/proj0/NBody.java
@@ -1,3 +1,34 @@
 public class NBody {
 
+    public static double readRadius(String planetsTxtPath) {
+        In in = new In(planetsTxtPath);
+        in.readInt();
+        return in.readDouble();
+    }
+
+    public static Planet[] readPlanets(String planetsTxtPath) {
+        In in = new In(planetsTxtPath);
+        int numberOfPlanets = in.readInt();
+        Planet[] planets = new Planet[numberOfPlanets];
+        double radius = in.readDouble();
+        for(int i = 0; i < numberOfPlanets; i++) {
+            double xP = in.readDouble();
+            double yP = in.readDouble();
+            double xV = in.readDouble();
+            double yV = in.readDouble();
+            double m = in.readDouble();
+            String img = in.readString();
+            planets[i] = new Planet(xP, yP, xV, yV, m, img);
+        }
+        return planets;
+    }
+
+    /*public static void main(String[] args) {
+        double T = args[0];
+        double dt = args[1];
+        String filename = args[3];
+        double universeRadius = readRadius(filename);
+
+    }*/
+
 }
