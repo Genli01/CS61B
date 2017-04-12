ad5fe80cdeed719246ad2cabc856b0d29fcea20a
Genli01 <ligen01@gmail.com>
2017-04-11 23:18:02 -0700

    Ctrated ArrayDeque and LinkedListDeque, started working on LinkedListDeque

---
+++ Projects/proj1a/ArrayDeque.java
@@ -0,0 +1,57 @@
+/**
+ * Created by Gen on 4/11/17.
+ */
+public class ArrayDeque {
+
+    public ArrayDeque() {
+
+    }
+
+    /* Adds an item to the front of the Deque. */
+    public void addFirst(Item) {
+
+    }
+
+    /* Adds an item to the front of the Deque. */
+    public void addLast(Item) {
+
+    }
+
+    /*Returns true if deque is empty, false otherwise. */
+    public boolean isEmpty() {
+
+    }
+
+    /* Returns the number of items in the Deque. */
+    public int size() {
+
+    }
+
+    /* Prints the items in the Deque from first to last,
+       separated by a space. */
+    public void printDeque() {
+
+    }
+
+    /* Removes and returns the item at the front of the Deque.
+    If no such item exists, returns null.*/
+    public Item removeFirst() {
+
+    }
+
+    /* Removes and returns the item at the back of the Deque.
+    If no such item exists, returns null.*/
+
+    public Item removeLast() {
+
+    }
+
+    /* Gets the item at the given index, where 0 is the front,
+    1 is the next item, and so forth. If no such item exists,
+    returns null. Must not alter the deque!*/
+
+    public Item get(int index) {
+
+    }
+
+}
